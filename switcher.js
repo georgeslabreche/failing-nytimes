4eb9443060e0b0800d9b6f21654589783db3e0d0
Georges Labrèche <georges@opendatakosovo.org>
2017-02-14 21:00:17 +0100

    added some regex rules

--- switcher.js
+++ switcher.js
@@ -8,7 +8,14 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/the nytimes/gi, 'the failing NYTimes');
+            var replacedText = text.replace(/The New York Times/g, 'The Failing New York Times');
+            replacedText = replacedText.replace(/The NY Times/g, 'The Failing NY Times');
+            replacedText = replacedText.replace(/The NYTimes/g, 'The Failing NYTimes');
+            replacedText = replacedText.replace(/the new york times/gi, 'the failing New York Times');
+            replacedText = replacedText.replace(/the nytimes/gi, 'the failing NYTimes');
+            replacedText = replacedText.replace(/the ny times/gi, 'the failing NY Times');
+            replacedText = replacedText.replace(/the nytimes/gi, 'the failing NYTimes');
+            replacedText = replacedText.replace(/NYT/gi, 'FYNT');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
